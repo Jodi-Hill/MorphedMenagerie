bce14e61c4490b68162f0e6019186ebdb5156039
JodiHill <jodi.hilhorst@student.hku.nl>
2026-05-06 16:50:34 +0200

    Enemies

--- Morphed Menagerie/Assets/Scripts/card game/System/MatchSetupSystem.cs
+++ Morphed Menagerie/Assets/Scripts/card game/System/MatchSetupSystem.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 
 public class MatchSetupSystem : MonoBehaviour
 {
-    [SerializeField] private List<CardDataTut> deckData;
+    [SerializeField] private HeroData heroData;
+    [SerializeField] private List<EnemyData> enemyDatas;
 
     private void Start()
     {
-        CardSystem.Instance.Setup(deckData);
+        HeroSystem.Instance.Setup(heroData);
+        EnemySystem.Instance.Setup(enemyDatas);
+        CardSystem.Instance.Setup(heroData.Deck);
         DrawCardsGA drawCardsGA = new(5);
         ActionSystem.Instance.Perform(drawCardsGA);
     }
