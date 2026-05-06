836d6c5be5dbb886e18bbd04c609f366db7f5b8e
JodiHill <jodi.hilhorst@student.hku.nl>
2026-05-06 12:18:28 +0200

    droparea

--- Morphed Menagerie/Assets/Scripts/card game/System/CardSystem.cs
+++ Morphed Menagerie/Assets/Scripts/card game/System/CardSystem.cs
@@ -17,6 +17,7 @@ public class CardSystem : Singleton<CardSystem>
     {
         ActionSystem.AttachPerformer<DrawCardsGA>(DrawCardsPerformer);
         ActionSystem.AttachPerformer<DiscardAllCardsGA>(DiscardAllCardsPerformer);
+        ActionSystem.AttachPerformer<PlayCardGA>(PlayCardPerformer);
         ActionSystem.SubscribeReaction<EnemyTurnGA>(EnemyTurnPreReaction, ReactionTiming.PRE);
         ActionSystem.SubscribeReaction<EnemyTurnGA>(EnemyTurnPostReaction, ReactionTiming.POST);
     }
@@ -25,6 +26,7 @@ public class CardSystem : Singleton<CardSystem>
     {
         ActionSystem.DetachPerformer<DrawCardsGA>();
         ActionSystem.DetachPerformer<DiscardAllCardsGA>();
+        ActionSystem.DetachPerformer<PlayCardGA>();
         ActionSystem.UnsubscribeReaction<EnemyTurnGA>(EnemyTurnPreReaction, ReactionTiming.PRE);
         ActionSystem.UnsubscribeReaction<EnemyTurnGA>(EnemyTurnPostReaction, ReactionTiming.POST);
     }
@@ -66,6 +68,16 @@ public class CardSystem : Singleton<CardSystem>
         }
         hand.Clear();
     }
+
+    private IEnumerator PlayCardPerformer(PlayCardGA playCardGA)
+    {
+        hand.Remove(playCardGA.Card);
+        CardView cardView = handView.RemoveCard(playCardGA.Card);
+        yield return DiscardCard(cardView);
+        // Perform effects
+    }
+
+    // Reactions
 
     private void EnemyTurnPreReaction(EnemyTurnGA enemyTurnGA)
     {
--- Morphed Menagerie/Assets/Scripts/card game/Views/CardView.cs
+++ Morphed Menagerie/Assets/Scripts/card game/Views/CardView.cs
@@ -8,8 +8,10 @@ public class CardView : MonoBehaviour
     [SerializeField] private TMP_Text mana;
     [SerializeField] private SpriteRenderer imageSR;
     [SerializeField] private GameObject wrapper;
-
+    [SerializeField] private LayerMask dropLayer;
     public Card Card { get; private set; }
+    private Vector3 dragStartPosition;
+    private Quaternion dragStartRotation;
 
     public void Setup(Card card)
     {
@@ -22,6 +24,7 @@ public class CardView : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (!Interactions.Instance.PlayerCanHover()) return;
         wrapper.SetActive(false);
         Vector3 pos = new(transform.position.x, -1, 0);
         CardViewHoverSystem.Instance.Show(Card, pos);
@@ -29,7 +32,42 @@ public class CardView : MonoBehaviour
 
     private void OnMouseExit()
     {
+        if (!Interactions.Instance.PlayerCanHover()) return;
         CardViewHoverSystem.Instance.Hide();
         wrapper.SetActive(true);
+    }
+
+    private void OnMouseDown()
+    {
+        if (!Interactions.Instance.PlayerCanInteract()) return;
+        Interactions.Instance.PlayerIsDragging = true;
+        wrapper.SetActive(true);
+        CardViewHoverSystem.Instance.Hide();
+        dragStartPosition = transform.position;
+        dragStartRotation = transform.rotation;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.position = MouseUtil.GetMousePositionInWorldSpace(-1);
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!Interactions.Instance.PlayerCanInteract()) return;
+        transform.position = MouseUtil.GetMousePositionInWorldSpace(-1);
+    }
+
+    private void OnMouseUp()
+    {
+        if (!Interactions.Instance.PlayerCanInteract()) return;
+        if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit, 10f, dropLayer))
+        {
+            PlayCardGA playCardGA = new(Card);
+            ActionSystem.Instance.Perform(playCardGA);
+        }
+        else
+        {
+            transform.position = dragStartPosition;
+            transform.rotation = dragStartRotation;
+        }
+        Interactions.Instance.PlayerIsDragging = false;
     }
 }
