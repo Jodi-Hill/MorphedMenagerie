20277e16f0578f6180df6b5b9e34789c0f853d54
JodiHill <jodi.hilhorst@student.hku.nl>
2026-05-06 17:28:26 +0200

    EnemyAttack

--- Morphed Menagerie/Assets/Scripts/card game/System/DamageSystem.cs
+++ Morphed Menagerie/Assets/Scripts/card game/System/DamageSystem.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class DamageSystem : MonoBehaviour
 {
-    private object knife;
-    private object health;
+    [SerializeField] private GameObject damageVFX;
 
     private void OnEnable()
     {
-//        ActionSystem.AttachPerformer<DealDamageGA>(DealDamagePerformer);
+        ActionSystem.AttachPerformer<DealDamageGA>(DealDamagePerformer);
     }
+
     private void OnDisable()
     {
         ActionSystem.DetachPerformer<DealDamageGA>();
     }
 
- //   private IEnumerator DealDamagePerformer(DealDamageGA dealDamageGA)
- //   {
- //       int damageAmount = dealDamageGA.Amount;
- //       Vector2 knifeStart = knife.transform.position;
- //       Tween tween = knife.transform.DOMove(health, transform.position, 0.25f);
- //       yield return tween.WaitForCompletion();
- //       knife.transform.DOMove(knifeStart, 0.5f);
- //       yield return health.ReduceHealth(damageAmount);
- //   }
+    private IEnumerator DealDamagePerformer(DealDamageGA dealDamageGA)
+    {
+        foreach (var target in dealDamageGA.Targets)
+        {
+            target.Damage(dealDamageGA.Amount);
+            Instantiate(damageVFX, target.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.15f);
+        }
+    }
 }
