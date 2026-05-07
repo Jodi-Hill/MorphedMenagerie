03bd5875a2f8b703f0838ba6724e957c4d68c2cc
JodiHill <jodi.hilhorst@student.hku.nl>
2026-05-07 12:38:31 +0200

    EnemyDeath

---
+++ Morphed Menagerie/Assets/Scripts/card game/Effects/DealDamageEffect.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DealDamageEffect : Effect
+{
+    [SerializeField] private int damageAmount;
+
+    public override GameAction GetGameAction()
+    {
+        List<CombatantView> targets = new(EnemySystem.Instance.Enemies);
+        DealDamageGA dealDamageGA = new(damageAmount, targets);
+        return dealDamageGA;
+    }
+}
--- Morphed Menagerie/Assets/Scripts/card game/System/EnemySystem.cs
+++ Morphed Menagerie/Assets/Scripts/card game/System/EnemySystem.cs
@@ -7,16 +7,20 @@ public class EnemySystem : Singleton<EnemySystem>
 {
     [SerializeField] private EnemyBoardView enemyBoardView;
 
+    public List<EnemyView> Enemies => enemyBoardView.EnemyViews;
+
     private void OnEnable()
     {
         ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer);
         ActionSystem.AttachPerformer<AttackHeroGA>(AttackHeroPerformer);
+        ActionSystem.AttachPerformer<KillEnemyGA>(KillEnemyPerformer);
     }
 
     private void OnDisable()
     {
         ActionSystem.DetachPerformer<EnemyTurnGA>();
         ActionSystem.DetachPerformer<AttackHeroGA>();
+        ActionSystem.DetachPerformer<KillEnemyGA>();
     }
 
     public void Setup(List<EnemyData> enemyDatas)
@@ -45,5 +49,10 @@ public class EnemySystem : Singleton<EnemySystem>
         attacker.transform.DOMoveX(attacker.transform.position.x + 1f, 0.25f);
         DealDamageGA dealDamageGA = new(attacker.AttackPower, new() { HeroSystem.Instance.HeroView });
         ActionSystem.Instance.AddReaction(dealDamageGA);
+    }
+
+    private IEnumerator KillEnemyPerformer(KillEnemyGA killEnemyGA)
+    {
+        yield return enemyBoardView.RemoveEnemy(killEnemyGA.EnemyView);
     }
 }
