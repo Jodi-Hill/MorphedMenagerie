d8ae08311d43a87614e0f336a059fa760c269654
JodiHill <jodi.hilhorst@student.hku.nl>
2026-06-10 02:51:57 +0200

    Build fixes

--- Morphed Menagerie/Assets/Scripts/SetCard.cs
+++ Morphed Menagerie/Assets/Scripts/SetCard.cs
@@ -5,10 +5,30 @@ public class SetCard : MonoBehaviour
     public CardStatistics activeCard;
     public InfoPanel infoPanel;
     private CardDetection cardDetection;
+    private bool selected;
 
     private void Start()
     {
         cardDetection = GetComponent<CardDetection>();
+    }
+
+    private void Update()
+    {
+        if (selected && Input.GetMouseButton(1) && activeCard != null)
+        {
+            infoPanel.gameObject.SetActive(true);
+            infoPanel.ShowInfoPanel(activeCard);
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        selected = true;
+    }
+
+    private void OnMouseExit()
+    {
+        selected = false;
     }
 
     public void NewCard (CardStatistics card)
@@ -19,6 +39,10 @@ public class SetCard : MonoBehaviour
 
     public bool HasBeenSet()
     {
+        if (cardDetection.firstTurnOnly && cardDetection.turn > 0)
+        {
+            return true;
+        }
         return cardDetection.hasCard;
     }
 
--- Morphed Menagerie/Assets/Scripts/card game/General/Util/MouseUtil.cs
+++ Morphed Menagerie/Assets/Scripts/card game/General/Util/MouseUtil.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 
 public static class MouseUtil 
 {
-    private static Camera camera = Camera.main;
     public static Vector3 GetMousePositionInWorldSpace(float zValue = 0f)
     {
-        Plane dragPlane = new(camera.transform.forward, new Vector3(0, 0, zValue));
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Plane dragPlane = new(Camera.main.transform.forward, new Vector3(0, 0, zValue));
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (dragPlane.Raycast(ray, out float distance))
         {
             return ray.GetPoint(distance);
