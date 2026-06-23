edccfab9b7fe00801140aafd45bb0871aff6c57e
JodiHill <jodi.hilhorst@student.hku.nl>
2026-06-23 14:50:22 +0200

    skip button added tutorial

--- Morphed Menagerie/Assets/Scripts/AfterVideoplayer.cs
+++ Morphed Menagerie/Assets/Scripts/AfterVideoplayer.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
@@ -6,10 +7,23 @@ public class AfterVideoplayer : MonoBehaviour
 {
     public RawImage rawImage;
     public VideoPlayer videoPlayer;
+    public Button button;
+
+    private bool buttonShown = false;
 
     private void Start()
     {
+        button.gameObject.SetActive(false);
         videoPlayer.loopPointReached += OnVideoFinished;
+    }
+
+    private void Update()
+    {
+        if (!buttonShown && videoPlayer.isPlaying)
+        {
+            button.gameObject.SetActive(true);
+            buttonShown = true;
+        }
     }
 
     private void OnVideoFinished(VideoPlayer vp)
